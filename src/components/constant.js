237c9e685fa6fac8494a88c65454cead390d5158
RegalRoy <regalroyacruz@gmail.com>
2023-08-16 11:53:14 -0700

    update3

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,4 +1,4 @@
-// const url = 'http://localhost:3000/book/'
-const url = 'https://finalsbe2.onrender.com/book/'
+const url = 'http://localhost:3000/book/'
+// const url = 'https://finalsbe2.onrender.com/book/'
 
 export default url;
